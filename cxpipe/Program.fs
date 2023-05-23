6e1d316e463c1f968155c49b03909163765d08e1
k1mlka <k1mlka@outlook.com>
2023-05-24 04:29:20 +0900

    .

--- cxpipe/Program.fs
+++ cxpipe/Program.fs
@@ -1,8 +1,13 @@
-﻿open System.IO
+﻿open System.Diagnostics
+open System.IO
+open System.Threading
 
 // This program should only compiled to win-x64 **AOT** and run by wine.
-// ep. `wine cxWrapper.exe pid`.
+// ep. `wine cxpipe.exe filename`.
 printfn "start"
+
+// "ぜったい征服☆学園結社パニャニャンダー!!"
+let filename = "ぜったい絶頂☆性器の大発明!!　─処女を狙う学園道具多発エロ─"
 
 let writer =
     try
@@ -16,11 +21,11 @@ let onDisconnect (processId: uint) : unit = ()
 let onCreateThread (param1: int64) (param2: uint) (param3: int64) (param4: int64) (param5: int64) (param6: string) (param7: string) : unit = ()
 let onRemoveThread (threadId: int64) : unit = ()
 let onOutputText (param1: int64) (text: string) (param3: uint) =
-    printfn $"{text}"
+    printfn $"{text} {Thread.CurrentThread.ManagedThreadId}"
+    writer.BaseStream.Position <- 0 
     writer.WriteLine(text)
     
-let proc = System.Diagnostics.Process.GetProcessesByName("ぜったい絶頂☆性器の大発明!!　─処女を狙う学園道具多発エロ─")[0]
-//let proc = System.Diagnostics.Process.GetProcessesByName("ぜったい征服☆学園結社パニャニャンダー!!")[0]
+let proc = Process.GetProcessesByName(filename)[0]
 printfn $"{proc.Id}"
 
 TextHostExport.TextHostInit(onConnect, onDisconnect, onCreateThread, onRemoveThread, onOutputText) |> ignore
@@ -28,4 +33,4 @@ TextHostExport.InjectProcess(uint proc.Id) |> ignore
 
 
 System.Console.ReadKey() |> ignore
-printfn "stop" // exit with game process
+printfn "stop" // only exit with game process
--- vnrplus/Main.fs
+++ vnrplus/Main.fs
@@ -1,5 +1,6 @@
 module Main
 
+open System.IO
 open Avalonia.Controls
 open Avalonia.FuncUI
 open Avalonia.FuncUI.DSL
@@ -7,30 +8,44 @@ open Avalonia.Layout
 
 let view () =
     Component(fun ctx ->
-        let state = ctx.useState 0
+        let state = ctx.useState "nothing new here"
 
         DockPanel.create [
             DockPanel.children [
                 Button.create [
                     Button.dock Dock.Bottom
-                    Button.onClick (fun _ -> state.Set(state.Current - 1))
-                    Button.content "-"
+                    Button.onClick (fun _ ->
+                        state.Set("waiting...")
+                        let fileWatcher = new FileSystemWatcher()
+                        fileWatcher.Path <- "/tmp/"
+                        fileWatcher.Filter <- "wine_out"
+                        fileWatcher.NotifyFilter <- NotifyFilters.LastWrite
+                        fileWatcher.EnableRaisingEvents <- true
+                        let handleFileChange _ (e: FileSystemEventArgs) =
+                            if e.ChangeType = WatcherChangeTypes.Changed then
+                                use fileStream = File.OpenRead e.FullPath
+                                use streamReader = new StreamReader(fileStream)
+                                let content = streamReader.ReadLine()
+                                state.Set(content)
+
+                        fileWatcher.Changed.AddHandler(FileSystemEventHandler(handleFileChange)))
+                    Button.content "Read begin"
                     Button.horizontalAlignment HorizontalAlignment.Stretch
                     Button.horizontalContentAlignment HorizontalAlignment.Center
                 ]
                 Button.create [
                     Button.dock Dock.Bottom
-                    Button.onClick (fun _ -> state.Set(state.Current + 1))
-                    Button.content "+"
+                    Button.onClick (fun _ -> ())
+                    Button.content "Stop"
                     Button.horizontalAlignment HorizontalAlignment.Stretch
                     Button.horizontalContentAlignment HorizontalAlignment.Center
                 ]
                 TextBlock.create [
                     TextBlock.dock Dock.Top
-                    TextBlock.fontSize 48.0
+                    TextBlock.fontSize 28.0
                     TextBlock.verticalAlignment VerticalAlignment.Center
                     TextBlock.horizontalAlignment HorizontalAlignment.Center
-                    TextBlock.text (string state.Current)
+                    TextBlock.text state.Current
                 ]
             ]
         ]
